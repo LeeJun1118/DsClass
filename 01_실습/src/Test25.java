c469a13a7bea13390754f81c454f3dd555f284c2
ljun1118 <ljun1118@ks.ac.kr>
2019-09-23 11:14:37 +0900

    Test25

--- 01_실습/src/Test25.java
+++ 01_실습/src/Test25.java
@@ -1,3 +1,6 @@
+import java.rmi.dgc.VMID;
+import java.util.Arrays;
+
 public class Test25 {
     public static void main(String[] args) {
         //정렬하고 시작
@@ -6,8 +9,22 @@ public class Test25 {
         // 길이 % 2 == 0 짝수 ,  길이 % 2 == 1 홀수
         // 짝수일때는 길이를 2로 나눈 위치와 그 위치 - 1 의 위치를 더해서 평균을 내야한다.
 
+        int n[] = {4,5,1,6,1,3,4,5,1};
 
+        Arrays.sort(n);
+        System.out.println(Arrays.toString(n));
+        int mid = (n.length)%2;
+        int m = (n.length)/2;
+        float middle = 0;
 
+        switch (mid){
+            case 0:
+                middle = (n[m] + n[m-1]);
+                System.out.println(middle);
+            case 1:
+                middle = n[m];
+                System.out.println(middle);
+        }
 
     }
 }
