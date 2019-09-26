1f17a2c44c82532091f13b294b9112c2486ef88c
ljun1118 <ljun1118@ks.ac.kr>
2019-09-26 22:15:13 +0900

    Test29

--- 01_실습/src/Test29.java
+++ 01_실습/src/Test29.java
@@ -2,35 +2,21 @@ import java.util.Arrays;
 
 public class Test29 {
     public static void main(String[] args) {
-        //Test18번 참고하기
-        //k = k%n.length
 
         int n[] = {1, 2, 3, 4, 5};
-        rev(n, 0, n.length - 1);
-    }
 
-    private static void rev(int[] n, int s, int e) {
-        //오른쪽 회전
-        for (int i = s; s < e; e--) {
-            System.out.println(Arrays.toString(n));
-            swap(n,s, e);
-            s++;
-            e--;
-            System.out.println(Arrays.toString(n));
+        System.out.println(n[n.length - 1]);
+        int k = 6;
+        for (int j = 0; j < k; j++) {
+
+            int temp = n[n.length - 1];
+
+            for (int i = n.length - 1; i > 0; i--) {
+                n[i] = n[i - 1];
+            }
+            n[0] = temp;
         }
-    }
-
-    private static void swap(int n[], int j, int k) {
-        for (int i = 0; i < j; i ++){
-
-        }
-
-        for (int i = j; i < k; j++){
-
-        }
-        int temp = n[j];
-        n[j] = n[k];
-        n[k] = temp;
+        System.out.println(Arrays.toString(n));
     }
 
 }
