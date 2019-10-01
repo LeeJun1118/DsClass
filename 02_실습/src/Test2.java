67ceee55900ca539170fd00cc98cb5d973231a50
ljun1118 <ljun1118@ks.ac.kr>
2019-10-01 19:21:36 +0900

    실습2

---
+++ 02_실습/src/Test2.java
@@ -0,0 +1,12 @@
+import java.util.LinkedList;
+
+public class Test2 {
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+        for (int i = 1; i <= 100; i++){
+            list.add(i);
+        }
+        System.out.println(list);
+
+    }
+}
