3aef7b087eb7f14154e86639471cd1c92db6c3ab
ljun1118 <ljun1118@ks.ac.kr>
2019-10-10 14:39:11 +0900

    8번

---
+++ 04_실습(Stack)/src/Test8.java
@@ -0,0 +1,12 @@
+import java.util.StringTokenizer;
+
+public class Test8 {
+    public static void main(String[] args) {
+        String e="2018-09-17 17:23:49";
+        StringTokenizer stok=new StringTokenizer(e, "- :");
+        //StringTokenizer stok=new StringTokenizer(e, "- :", true);
+        while (stok.hasMoreTokens()) {
+            System.out.println(stok.nextToken());
+        }
+    }
+}
