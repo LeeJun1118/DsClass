526a8e35ba97b62eae4b89ca9b553f84489d5b51
ljun1118 <ljun1118@ks.ac.kr>
2019-10-10 14:52:10 +0900

    10번ing

---
+++ 04_실습(Stack)/src/Test10.java
@@ -0,0 +1,16 @@
+import java.util.Stack;
+
+public class Test10 {
+    public static void main(String[] args) {
+        String e = "3 1 2 / *";
+        System.out.println(postfixEval(e));
+    }
+
+    private static double postfixEval(String e) {
+        Stack<Double> stack = new Stack<>();
+        for (String token : e.split("\\s+")) {
+            stack.push(token);
+        }
+        return stack.pop();
+    }
+}
