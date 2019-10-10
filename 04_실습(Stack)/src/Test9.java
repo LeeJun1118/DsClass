e3b2aa70ebbddfe9cfb37165fb50a5cffa6c7b4f
ljun1118 <ljun1118@ks.ac.kr>
2019-10-10 14:51:57 +0900

    9번

--- 04_실습(Stack)/src/Test9.java
+++ 04_실습(Stack)/src/Test9.java
@@ -2,8 +2,8 @@ import java.util.Arrays;
 
 public class Test9 {
     public static void main(String[] args) {
-        String e="3 1 2 / *";
-        String v[]=e.split("\\s+");
+        String e = "3 1 2 / *";
+        String v[] = e.split("\\s+");
         System.out.println(Arrays.toString(v));
     }
 }
