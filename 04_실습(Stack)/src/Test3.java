993312dd1e8f63b6e0def620e53dbc3dbe7483a8
ljun1118 <ljun1118@ks.ac.kr>
2019-10-14 10:25:43 +0900

    modify

--- 04_실습(Stack)/src/Test3.java
+++ 04_실습(Stack)/src/Test3.java
@@ -4,15 +4,10 @@ public class Test3 {
     public static void main(String[] args) {
         String s = "대한민국의 수도는 서울이다.";
         Stack<Character> stack = new Stack<>();
-        for (int i = 0; i < s.length(); i++)
-        {
+        for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
             stack.push(c);
         }
-        for (int i = 0; i < s.length(); i++)
-        {
-            System.out.println(stack.pop());
-        }
-
+        while (!stack.isEmpty()) System.out.print(stack.pop());
     }
 }
