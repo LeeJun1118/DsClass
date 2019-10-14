7f965793fbd99cb5b31277f1e1f193071ba581d4
ljun1118 <ljun1118@ks.ac.kr>
2019-10-14 10:20:57 +0900

    complete

---
+++ 04_실습(Stack)/src/Test13.java
@@ -0,0 +1,17 @@
+public class Test13 {
+    public static void main(String[] args) {
+        String s = "대한민국의 수도는 서울이다.";
+        char stack[] = new char[s.length()];
+        int top = -1;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            stack[++top] = c;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            System.out.print(stack[top--]);
+        }
+
+    }
+}
