38257b13ae11e80daaa64a853b3de7e8d9a490d1
ljun1118 <ljun1118@ks.ac.kr>
2019-10-17 14:12:12 +0900

    요새푸스 문제

--- 05_실습(큐)/src/Test4_2.java
+++ 05_실습(큐)/src/Test4_2.java
@@ -1,5 +1,29 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Test4_2 {
     public static void main(String[] args) {
+        System.out.println(solveJosephusProblem(7, 3));
+    }
 
+    private static int solveJosephusProblem(int n, int k) {
+        LinkedList<Integer> queue = new LinkedList<>();
+
+        for (int i = 1; i <= n; i++) {
+            queue.addLast(i);
+            System.out.println(queue);
+        }
+
+        while (queue.size() > 1) {
+
+            for (int i = 0; i < k - 1; i++) {
+                int v = queue.removeFirst();
+                queue.addLast(v);
+            }
+            queue.removeFirst();
+        }
+
+
+        return queue.removeFirst();
     }
 }
