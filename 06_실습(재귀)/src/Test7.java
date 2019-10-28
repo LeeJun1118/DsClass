544d7fe256823634d82b6c8e58fb5f19dc8dba48
ljun1118 <ljun1118@ks.ac.kr>
2019-10-28 11:09:41 +0900

    재귀7

---
+++ 06_실습(재귀)/src/Test7.java
@@ -0,0 +1,12 @@
+public class Test7 {
+    public static void main(String[] args) {
+        int n[] = {1,2,3,4,5};
+        System.out.println(sum(n,0));
+
+    }
+
+    private static int sum(int[] n, int i) {
+        if (i == n.length-1) return n[n.length-1];
+        return n[i] + sum(n,i+1);
+    }
+}
