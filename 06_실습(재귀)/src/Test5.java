7a66634f74f5397042cbe5488a9421321b2cf5a2
ljun1118 <ljun1118@ks.ac.kr>
2019-10-28 10:54:34 +0900

    재귀5

--- 06_실습(재귀)/src/Test5.java
+++ 06_실습(재귀)/src/Test5.java
@@ -1,10 +1,10 @@
 public class Test5 {
     public static void main(String[] args) {
-        System.out.println(power(2,60));
+        System.out.println(power(2,10));
     }
 
     private static long power(long m, long n) {
         if (n == 0) return 1;
-        return power(m*m,n/2)*(n%2==0?1:m);
+        return power(m*m,n/2)*(n%2==0? 1:m);
     }
 }
