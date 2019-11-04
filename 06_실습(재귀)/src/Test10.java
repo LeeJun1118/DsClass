fe683f9608397b9ef83c904f70a615821eb1df7b
ljun1118 <ljun1118@ks.ac.kr>
2019-11-04 10:59:53 +0900

    미완성

--- 06_실습(재귀)/src/Test10.java
+++ 06_실습(재귀)/src/Test10.java
@@ -1,17 +1,24 @@
+/*
 public class Test10 {
     public static void main(String[] args) {
         //대문자로 바꾸기
         //소문자인지 검사
         //v[1] >= 'a' && v[1] <= 'z'
         //대문자로 바꾸려면 'a' - 32 = 'A'
-        String s="South, Korea";
-        char v[]=s.toCharArray();
+        String s = "South, Korea";
+        char v[] = s.toCharArray();
         System.out.println(countUpper(v, 0));
     }
 
     private static int countUpper(char[] v, int i) {
         //i부터 끝까지
+        if (i == v.length - 1) return v[i];
 
-        return 0;
+        if (v[i] >= 'a' && v[i] <= 'z')
+            //char c = v[i];
+
+
+        return countUpper(v, i + 1);
     }
 }
+*/
--- 06_실습(재귀)/src/Test11.java
+++ 06_실습(재귀)/src/Test11.java
@@ -1,5 +1,16 @@
 public class Test11 {
     public static void main(String[] args) {
 
+        String s = "Korea";
+        char v[] = s.toCharArray();
+
+        char c = v[0];
+
+        System.out.println(c);
+        c = (char) (c - 32);
+
+        System.out.println(c);
+
+        System.out.println(v[0]);
     }
 }
