ef5110c7b5fdc76d4ee253adc3a9d9929d3c643f
ljun1118 <ljun1118@ks.ac.kr>
2019-11-04 10:33:54 +0900

    회문

--- 06_실습(재귀)/src/Test12.java
+++ 06_실습(재귀)/src/Test12.java
@@ -1,5 +1,12 @@
 public class Test12 {
     public static void main(String[] args) {
+        String s = "ABCDEDCBA";  // "기러기"
+        System.out.println(palindrome(s, 0));
+    }
 
+    private static boolean palindrome(String s, int i) {
+        if (i == s.length()/2) return true;
+        if (s.charAt(i)!=s.charAt(s.length()-1-i)) return false;
+        return palindrome(s,i+1);
     }
 }
