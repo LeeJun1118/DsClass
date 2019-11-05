0e01e9c6a612a38a02c3645465858d6926030528
ljun1118 <ljun1118@ks.ac.kr>
2019-11-05 20:01:26 +0900

    10완료

--- 06_실습(재귀)/src/Test10.java
+++ 06_실습(재귀)/src/Test10.java
@@ -1,24 +1,17 @@
-/*
 public class Test10 {
     public static void main(String[] args) {
-        //대문자로 바꾸기
-        //소문자인지 검사
-        //v[1] >= 'a' && v[1] <= 'z'
-        //대문자로 바꾸려면 'a' - 32 = 'A'
-        String s = "South, Korea";
+        String s = "South, Korea 안녕";
         char v[] = s.toCharArray();
-        System.out.println(countUpper(v, 0));
+        toUpper(v, 0);
+        System.out.println(v);
     }
 
-    private static int countUpper(char[] v, int i) {
-        //i부터 끝까지
+    private static int toUpper(char[] v, int i) {
         if (i == v.length - 1) return v[i];
-
         if (v[i] >= 'a' && v[i] <= 'z')
-            //char c = v[i];
+            v[i] = (char) (v[i] - 32);
 
-
-        return countUpper(v, i + 1);
+        return toUpper(v, i + 1);
     }
 }
-*/
+
