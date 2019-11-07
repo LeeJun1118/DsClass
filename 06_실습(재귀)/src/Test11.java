dab9f152af176a2cc919ad39c564308dd0db7d21
ljun1118 <ljun1118@ks.ac.kr>
2019-11-07 14:50:33 +0900

    11완성

--- 06_실습(재귀)/src/Test11.java
+++ 06_실습(재귀)/src/Test11.java
@@ -1,14 +1,15 @@
 public class Test11 {
     public static void main(String[] args) {
-        String s = "SoutDDh, Korea";
+        String s = "South, Korea";
         char v[] = s.toCharArray();
         System.out.println(countUpper(v, 0));
     }
 
     private static int countUpper(char[] v, int i) {
-        if (i == v.length ) return v[i];
-        if (v[i] >= 'a' && v[i] <= 'z')
-            return v[i];
-        return countUpper(v, i + 1);
+        if (i == v.length) return 0;
+        if (v[i] >= 'A' && v[i] <= 'Z')
+            return 1 + countUpper(v, i + 1);
+        else
+            return 0 + countUpper(v, i + 1);
     }
 }
