dac58f41ab92b936f04ab4655aaf7316c360a616
ljun1118 <ljun1118@ks.ac.kr>
2019-11-18 10:38:30 +0900

    이진탐색 1-2

--- 08_실습(이진탐색)/src/Test1_2.java
+++ 08_실습(이진탐색)/src/Test1_2.java
@@ -1,23 +1,25 @@
-import java.util.Arrays;
+import java.util.Random;
 
 public class Test1_2 {
     public static void main(String[] args) {
-        int v[]= {1,1,1,1,2,2,2,3,4,5,8,9,9,9,9,9};
-        Arrays.sort(v); // 배열 정렬
-        int key=7;
-        int index=binarySearch(v, key);
-        if(index<0) System.out.println("배열 내 자료 없음");
-        else System.out.println("자료 발견 위치(배열 내 index): "+index);
-    }
-    private static int binarySearch(int[] v, int key) {
-        int left=0, right=v.length-1;
-        while(left<=right){
-            int m=(left+right)/2;
-            if(key==v[m]) return m;
-            if(key<v[m]) right=m-1;
-            else left=m+1;
-        }
-        return -1;
+        int v[] = new int[1000000];
+        Random random = new Random();
+        for (int i = 0; i < v.length; i++) v[i] = random.nextInt(1000000);
+        int key = 1234;
+        System.out.println(search(v, key));
     }
 
+    private static int search(int[] v, int key) {
+        int i = 0;
+        int lastValue = v[v.length - 1];
+        v[v.length - 1] = key;
+
+        while (v[i] != key) i++;
+        v[v.length-1] = lastValue; //복원
+
+        if (i < v.length - 1) return i;
+
+        if (key == lastValue) return v.length - 1;
+        else return -1;
+    }
 }
